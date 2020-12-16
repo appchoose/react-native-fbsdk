6971ca9b7e690d819444e78127f7bb23e5ff9263
thibaut <thibaut.gensollen@gmail.com>
2020-12-16 13:54:41 +0100

    change amplitude ios

--- ios/RCTFBSDK/core/RCTFBSDKAppLink.m
+++ ios/RCTFBSDK/core/RCTFBSDKAppLink.m
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-#import "RCTFBSDKAppLink.h"
-#import "React/RCTLog.h"
-#import <FBSDKCoreKit/FBSDKCoreKit.h>
-#import <FBSDKLoginKit/FBSDKLoginKit.h>
-#import <FBSDKShareKit/FBSDKShareKit.h>
-#import "React/RCTLog.h"
-
-@implementation RCTFBSDKAppLink{
-  NSURL *appLinkUrl;
-}
-// This RCT (React) "macro" exposes the current module to JavaScript
-RCT_EXPORT_MODULE(FBAppLink);
-
-RCT_REMAP_METHOD(get,
-                 resolver:(RCTPromiseResolveBlock)resolve
-                 rejecter:(RCTPromiseRejectBlock)reject)
-{
-  dispatch_async(dispatch_get_main_queue(), ^{
-    [FBSDKAppLinkUtility fetchDeferredAppLink:^(NSURL *url, NSError *error) {
-       if (error) {
-        RCTLogInfo(@"FBAppLink Received error while fetching deferred app link %@", error);
-          reject(@"error", @"There were an error", error);
-      }
-      if (url) {
-      self->appLinkUrl = url;
-      [[UIApplication sharedApplication] openURL:url];
-      NSString* volumeString = [NSString stringWithFormat:@"%@", self->appLinkUrl];
-      resolve(volumeString);
-      }else{
-        resolve(url);
-      }
-    }];
-  });
-}
-
-@end
-=======
 // Copyright (c) 2015-present, Facebook, Inc. All rights reserved.
 //
 // You are hereby granted a non-exclusive, worldwide, royalty-free license to use,
@@ -88,4 +50,3 @@ RCT_REMAP_METHOD(fetchDeferredAppLink,
 }
 
 @end
->>>>>>> 4577507a864baab99d26270277a7a8a34e178c42
