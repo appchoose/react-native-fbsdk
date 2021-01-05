b226334d2e662783049e1732e0187aecca43bad0
thibaut <thibaut.gensollen@gmail.com>
2021-01-05 13:38:58 +0100

    back to deferred

--- ios/RCTFBSDK/core/RCTFBSDKAppLink.m
+++ ios/RCTFBSDK/core/RCTFBSDKAppLink.m
@@ -1,21 +1,3 @@
-// Copyright (c) 2015-present, Facebook, Inc. All rights reserved.
-//
-// You are hereby granted a non-exclusive, worldwide, royalty-free license to use,
-// copy, modify, and distribute this software in source code or binary form for use
-// in connection with the web services and APIs provided by Facebook.
-//
-// As with any software that integrates with the Facebook platform, your use of
-// this software is subject to the Facebook Developer Principles and Policies
-// [http://developers.facebook.com/policy/]. This copyright notice shall be
-// included in all copies or substantial portions of the software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
 #import "RCTFBSDKAppLink.h"
 
 #import <React/RCTUtils.h>
@@ -41,9 +23,9 @@ RCT_REMAP_METHOD(fetchDeferredAppLink,
           reject(@"error", @"There were an error", error);
       }
       if (url) {
-      self->appLinkUrl = url;
+      NSString* appLinkUrl = url;
       [[UIApplication sharedApplication] openURL:url];
-      NSString* volumeString = [NSString stringWithFormat:@"%@", self->appLinkUrl];
+      NSString* volumeString = [NSString stringWithFormat:@"%@", appLinkUrl];
       resolve(volumeString);
       }else{
         resolve(url);
